1fe117d31e5f6186bed87ad0ce40b7ce3c7e505e
ugurekc <ugur.ekc@gmail.com>
2024-02-29 16:40:31 +0300

    Initial commit.

---
+++ MvcWeb4/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWeb4.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+             ViewBag.list1 = new SelectListItem[]{
+             new SelectListItem(){Text="İstanbul", Value="i1"},
+             new SelectListItem(){Text="Ankara", Value="a1"},
+             new SelectListItem(){Text="İzmir", Value="i2"}
+            };
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string text1, string check1, string list1)
+        {
+            ViewBag.Message = "Your application description page.";
+            
+            return View();
+        }
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcWeb4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcWeb4.MvcApplication" Language="C#" %>
